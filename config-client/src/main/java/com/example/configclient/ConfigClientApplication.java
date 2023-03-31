bce5523f92afe2c763e4d7bffd004de81c9b2bcc
viplav.fauzdar <viplav.fauzdar@gmail.com>
2023-03-31 18:12:49 -0400

    multiple profiles

--- config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
+++ config-client/src/main/java/com/example/configclient/ConfigClientApplication.java
@@ -2,7 +2,9 @@ package com.example.configclient;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Profile;
 
+@Profile("qa") //set in env variable spring_active_profile
 @SpringBootApplication
 public class ConfigClientApplication {
 
